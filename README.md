be5c28e1791157c2a4c3e46e591c9f735d241043
Justin J Daniel <62233773+Justinjdaniel@users.noreply.github.com>
2021-06-07 10:11:58 +0530

    :sparkles:  files added

--- README.md
+++ README.md
@@ -1,2 +1,25 @@
-# zsh-Terminal-customization
+# ✨ zsh-Terminal-customization
 Oh-my-zsh : Tewrminal customizations 
+
+## Usage
+### First Time Installation
+````zsh
+git clone https://github.com/Justinjdaniel/zsh-Terminal-customization.git
+cd ./zsh-Terminal-customization
+./install.sh
+````
+
+### Update Already Installed Files
+````zsh
+cd ./zsh-Terminal-customization
+./install.sh -u
+````
+
+**For Reference:** https://github.com/ohmyzsh/ohmyzsh/
+
+**Add more plugins wisely :**
+- https://github.com/ohmyzsh/ohmyzsh/tree/master/plugins
+- https://github.com/zsh-users
+- https://github.com/unixorn/awesome-zsh-plugins
+- 
+**Powerline Fonts:** https://github.com/powerline/fonts
---
+++ install.sh
@@ -0,0 +1,48 @@
+#!/usr/bin/env zsh
+
+function installDependencies() {
+  sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+  git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k;
+  git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions;
+  git clone https://github.com/zsh-users/zsh-syntax-highlighting ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting;
+
+  echo "👌 dependencies installed";
+}
+
+function configureZsh() {
+  cp ./.zshrc ~/.zshrc;
+
+  echo "👌 zsh configured successfully";
+}
+
+function configureGit() {
+  cp ./.gitconfig ~/.gitconfig;
+  cp ./.gitignore_global ~/.gitignore_global;
+
+  echo "👌 git configured successfully";
+}
+
+function configureVim() {
+  cp ./.vimrc ~/.vimrc;
+
+  echo "👌 vim configured successfully";
+
+}
+
+function doIt() {
+  if [ "$1" != "-u" ]; then
+      installDependencies;
+  fi
+  
+  configureZsh;
+  configureGit;
+  configureVim;
+  echo "✅ Done";
+}
+
+trap 'echo "❌ an error occured" && exit' ERR
+
+doIt $1;
+
+unset installDependencies doIt configureZsh configureGit configureVim;
